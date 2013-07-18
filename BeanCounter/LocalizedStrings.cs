dd059a2cfe45fa10ccb4035162ad06d25a64fa8e
Ben Andrews <ben.andrews@gmail.com>
2013-07-17 21:42:41 -0700

    Initial Check-In

---
+++ BeanCounter/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BeanCounter.Resources;
+
+namespace BeanCounter
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
