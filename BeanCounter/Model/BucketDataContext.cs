2d894f5b504bf0f2f66cae425a87f98a2cbf5b4f
Ben Andrews <ben.andrews@gmail.com>
2013-08-11 18:12:16 -0700

    Added scroll view to main page & Bucket view model

--- BeanCounter/Model/BucketDataContext.cs
+++ BeanCounter/Model/BucketDataContext.cs
@@ -5,13 +5,76 @@ using System.Data.Linq.Mapping;
 
 namespace BeanCounter.Model
 {
+
+    public class BucketDataContext : DataContext
+    {
+        // Pass the connection string to the base class.
+        public BucketDataContext(string connectionString)
+            : base(connectionString)
+        { }
+
+        // Specify a table for the to-do items.
+        public Table<Bucket> Buckets;
+    }
+
+
     [Table]
     public class Bucket : INotifyPropertyChanged, INotifyPropertyChanging
     {
 
-        //
-        // TODO: Add columns and associations, as applicable, here.
-        //
+        // Define ID: private field, public property, and database column.
+        private int _bucketItemId;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int BucketItemId
+        {
+            get { return _bucketItemId; }
+            set
+            {
+                if (_bucketItemId != value)
+                {
+                    NotifyPropertyChanging("BucketItemId");
+                    _bucketItemId = value;
+                    NotifyPropertyChanged("BucketItemId");
+                }
+            }
+        }
+
+        // Define item name: private field, public property, and database column.
+        private string _bucketName;
+
+        [Column]
+        public string BucketName
+        {
+            get { return _bucketName; }
+            set
+            {
+                if (_bucketName != value)
+                {
+                    NotifyPropertyChanging("BucketName");
+                    _bucketName = value;
+                    NotifyPropertyChanged("BucketName");
+                }
+            }
+        }
+
+        // Define completion value: private field, public property, and database column.
+        private int _beanCount;
+
+        [Column]
+        public int BeanCount
+        {
+            get { return _beanCount; }
+            set
+            {
+                if (_beanCount != value)
+                {
+                    NotifyPropertyChanging("BeanCount");
+                    _beanCount = value;
+                    NotifyPropertyChanged("BeanCount");
+                }
+            }
+        }
 
         // Version column aids update performance.
         [Column(IsVersion = true)]
---
+++ BeanCounter/ViewModel/BucketViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+using BeanCounter.Model;
+
+namespace BeanCounter.ViewModel
+{
+    public class BucketViewModel : INotifyPropertyChanged
+    {
+        // LINQ to SQL data context for the local database.
+        private BucketDataContext bucketDB;
+
+        // Class constructor, create the data context object.
+        public BucketViewModel(string bucketDBConnectionString)
+        {
+            bucketDB = new BucketDataContext(bucketDBConnectionString);
+        }
+
+        // All bcket items.
+        private ObservableCollection<Bucket> _allBuckets;
+        public ObservableCollection<Bucket> AllBuckets
+        {
+            get { return _allBuckets; }
+            set
+            {
+                _allBuckets = value;
+                NotifyPropertyChanged("AllBuckets");
+            }
+        }
+
+        // Write changes in the data context to the database.
+        public void SaveChangesToDB()
+        {
+            bucketDB.SubmitChanges();
+        }
+
+
+        // Query database and load the collections and list used by the pivot pages.
+        public void LoadCollectionsFromDatabase()
+        {
+            // Specify the query for all buckets in the database.
+            var bucketsInDB = from Bucket buckets in bucketDB.Buckets
+                                select buckets;
+
+            // Query the database and load all buckets.
+            AllBuckets = new ObservableCollection<Bucket>(bucketsInDB);
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the app that a property has changed.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
